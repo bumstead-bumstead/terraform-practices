63ca6054258b605b9cb6117647a6061e0f39a7b7
이요환 <91039622+bumstead-bumstead@users.noreply.github.com>
2023-04-07 16:44:26 +0900

    write Output components example

---
+++ learn-terraform-aws-instance/variables.tf
@@ -0,0 +1,6 @@
+
+variable instance_name {
+  type        = string
+  default     = "ExampleAppServerInstance"
+  description = "value of ec2 instacne name"
+}
--- query-data-with-output/main.tf
+++ query-data-with-output/main.tf
@@ -10,17 +10,23 @@ terraform {
   required_version = ">= 1.2.0"
 }
 
-
-
 provider "aws" {
-  region = "us-west-2"
+  region  = "us-west-2"
 }
 
 resource "aws_instance" "app_server" {
-  ami           = "ami-830c94e3"
+  ami           = "ami-08d70e59c07c61a3a"
   instance_type = "t2.micro"
 
   tags = {
     Name = var.instance_name
   }
+}
+
+# variables.tf
+
+variable "instance_name" {
+  description = "Value of the Name tag for the EC2 instance"
+  type        = string
+  default     = "ExampleAppServerInstance"
 }
