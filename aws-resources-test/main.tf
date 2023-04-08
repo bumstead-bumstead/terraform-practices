de2a59cf6afed0bad57f72803d0944241c686c2b
이요환 <91039622+bumstead-bumstead@users.noreply.github.com>
2023-04-08 15:31:35 +0900

    aws resources test - S3 설정

--- aws-resources-test/main.tf
+++ aws-resources-test/main.tf
@@ -72,3 +72,23 @@ resource "aws_instance" "myec2" {
     Name = "myec2"
   }
 }
+
+resource "aws_s3_bucket" "mybucket" {
+    bucket = "yohwan-example-bucket"
+
+#   subnet_id = aws_subnet.example_subnet.id
+
+    tags = {
+        Name = "mybucket"
+        Environment = "test"
+    }
+}
+
+resource "aws_s3_access_point" "vpc_access_point" {
+  bucket = aws_s3_bucket.mybucket.id
+  name   = "yohwan-vpc-access-point"
+
+  vpc_configuration {
+    vpc_id = aws_vpc.main.id
+  }
+}
