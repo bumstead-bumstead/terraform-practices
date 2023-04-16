21af38e610f4df9cbe176791548d97b0d48042e1
이요환 <91039622+bumstead-bumstead@users.noreply.github.com>
2023-04-17 02:01:35 +0900

    s3에 public access policy 추가

--- aws-resources-test/s3.tf
+++ aws-resources-test/s3.tf
@@ -9,6 +9,27 @@ resource "aws_s3_bucket" "mybucket" {
     }
 }
 
+resource "aws_s3_bucket_policy" "public_access_policy" {
+  bucket = aws_s3_bucket.mybucket.id
+  policy = jsonencode({
+    Id = "testBucketPolicy"
+    Statement = [
+      {
+        Action = "s3:GetObject"
+        Effect = "Allow"
+        Principal = "*"
+        Resource = [
+          aws_s3_bucket.mybucket.arn,
+          "${aws_s3_bucket.mybucket.arn}/*",
+        ]
+        Sid      = "statement1"
+      }
+    ]
+    Version = "2012-10-17"
+  })
+}
+
+
 resource "aws_s3_access_point" "vpc_access_point" {
   bucket = aws_s3_bucket.mybucket.id
   name   = "yohwan-vpc-access-point"
